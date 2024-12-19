2ff1eb17310a2b11dcc36f09900efdbf62c6beef
Suhejl Vejseli <suhejl.vejseli03@gmail.com>
2024-12-20 00:20:22 +0100

    Cluster-Networking: setup basic networking a private and public subnets

---
+++ 02-cluster/02-networking/main.tf
@@ -0,0 +1,107 @@
+locals {
+  project = "terraform-aws-k8s-cluster"
+
+  default_tags = {
+    Project = local.project
+    Label   = "Thesis"
+  }
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = merge({
+    Name = "${local.project}-main-VPC"
+  }, local.default_tags)
+}
+
+resource "aws_subnet" "public" {
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index + 2)
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+
+  tags = merge({
+    Name = "public-subnet-${count.index + 1}"
+    Tier = "Public"
+  }, local.default_tags)
+}
+
+resource "aws_subnet" "private" {
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 10, count.index + 2)
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+
+  tags = merge({
+    Name = "pivate-subnet-${count.index + 1}"
+    Tier = "Private"
+  }, local.default_tags)
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge({
+    Name = "${local.project}-internet-gateway"
+  }, local.default_tags)
+}
+
+resource "aws_eip" "nat_gtw_eip" {
+  domain = "vpc"
+
+  tags = merge({
+    Name = "${local.project}-nat-gtw-eip"
+  }, local.default_tags)
+}
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat_gtw_eip.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = merge({
+    Name = "${local.project}-nat-gtw"
+  }, local.default_tags)
+}
+
+# public route table, route internet traffic to internet gateway
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = merge({
+    Name = "${local.project}-public-route-table"
+  }, local.default_tags)
+}
+
+# associate public route table with public subnet
+resource "aws_route_table_association" "public_association" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+# private route table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
+
+  tags = merge({
+    Name = "${local.project}-private-route-table"
+  }, local.default_tags)
+}
+
+# associate private route table with private subnet
+resource "aws_route_table_association" "private_association" {
+  count          = length(aws_subnet.private)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
