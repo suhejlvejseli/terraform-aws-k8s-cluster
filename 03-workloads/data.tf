6e0d8a2c9ba2125631cf1765e42428d0da73ff29
Suhejl Vejseli <suhejl.vejseli03@gmail.com>
2025-01-09 15:47:05 +0100

    fix shared state issue

--- 03-workloads/data.tf
+++ 03-workloads/data.tf
@@ -3,7 +3,7 @@ data "terraform_remote_state" "networking" {
 
   config = {
     bucket = "terraform-aws-k8s-cluster-terraform-state"
-    key    = "02-workloads/02-networking/terraform.tfstate"
+    key    = "02-cluster/02-networking/terraform.tfstate"
     region = "eu-central-1"
     dynamodb_table = "terraform-aws-k8s-cluster-terraform-lock"
   }
